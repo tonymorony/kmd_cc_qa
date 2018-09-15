f58ee7839b9d87bbcc18c5eb09eef049b717a69d
Anton Lysakov <tlysakov@gmail.com>
2018-09-15 13:10:30 +0700

    working version of oracle mass creation tool (create, register, make utxos)

--- oracle_mass_creation.py
+++ oracle_mass_creation.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 import json
+import time
 from subprocess import check_output
 from test_modules import file_oraclize
 
 ac_name = input("Input AC name with which you want to work (exmp: ORCL): ")
 oracles_number = int(input("Input how many oracles do you want to create: "))
 oracles_type = input("Set your oracle type (e.g. d): ")
+utxos_number = int(input("How many UTXOs you want to create for your oracles: "))
+pubkey = input("Input your pubkey (for publisher id): ")
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def oracle_create(ac_name, name, description, type):
     new_oracle = json.loads(check_output(
@@ -40,34 +53,25 @@ while oracles_number > 0:
     file1.writelines(new_oracle + "\n")
     oracles_number = oracles_number - 1
 file1.close()
-print("Oracles succesfully created. IDs can be found in oracles_list file")
+print("Oracles succesfully created. IDs can be found in oracles_list file.")
 
 file = open("oracles_list", "r")
 file2 = open("register_list", "w")
 for line in file:
-    new_registration = oracle_register(ac_name, line, "100000")
+    new_registration = oracle_register(ac_name, line.rstrip(), "100000")
     file2.writelines(new_registration + "\n")
 file2.close()
 file.close()
-print("Oracles registration txs broadcasted. Baton TX IDs can be found in register_list file")
+print("Oracles registration txs broadcasted. Baton TX IDs can be found in register_list file.")
 
 file = open("oracles_list", "r")
 file3 = open("register_list", "r")
-file4 = open("subscriptions_list", "w")
+file4 = open("subscriptions_list", "a")
 for line1, line2 in zip(file, file3):
-    new_subscription = oracle_subscribe(ac_name, line1, line2, "10000")
-    file4.writelines(new_subscription + "\n")
+    for i in range(utxos_number):
+        new_subscription = oracle_subscribe(ac_name, line1.rstrip(), pubkey, "10000")
+        file4.write(new_subscription + "\n")
 file4.close()
-print("Oracles subscribtion txs broadcasted. Subscription TX IDs can be found in subscriptions_list file")
+print("Oracles subscribtion txs broadcasted. Subscription TX IDs can be found in subscriptions_list file.")
 
-# subscribe on all oracles
-
-# checking if subscribed on all oracles
-
-# print("Preparation done. Ready to publish the data")
-
-# choosing file to publish
-
-#2) run concurent data uploading to X oracles
-#3) grab data from X oracles
-#4) compare data for X files
+print(bcolors.OKGREEN + "Preparation done. Ready to publish the data"  + bcolors.ENDC)
